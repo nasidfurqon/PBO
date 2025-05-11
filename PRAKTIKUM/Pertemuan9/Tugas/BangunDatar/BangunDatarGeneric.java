aae720e00df2aefacf0376671b78dcfec196cf00
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-05-11 11:03:19 +0700

    Identitas di file BangunDatarGeneric.java

--- PRAKTIKUM/Pertemuan9/Tugas/BangunDatar/BangunDatarGeneric.java
+++ PRAKTIKUM/Pertemuan9/Tugas/BangunDatar/BangunDatarGeneric.java
@@ -1,3 +1,7 @@
+// Pembuat      : Khoirul Nasid Furqon - 24060123120042
+// Tanggal      : 10 Mei 2025
+// File         : BangunDatarGenericTest.java
+// Deskripsi    : kelas konstruksi generic untuk BangunDatar
 
 public class BangunDatarGeneric<T extends BangunDatar> {
     private T bangunDatar;
